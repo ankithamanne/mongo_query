2ed259b4ad7f75e06c13d66d82e6c9c8d8079141
ankithamanne <ankithamanne2003@gmail.com>
2025-04-22 11:30:36 +0530

    s

--- q16.js
+++ q16.js
@@ -24,9 +24,48 @@ db.employees.aggregate([
             grade:{
                 //cond:[condition, if-true, if-false]
                 $cond:[{
-                    $gte:["$salary", 1500]}, 
+                    $gte:["$salary", 1500]}, //gte = greater than or equal to
                     "Grade A", 
                     "Grade B"],
+            },
+        },
+    },
+]);
+
+
+db.employees.aggregate([
+    {
+        $project:{
+            _id: 0,
+            name:1,
+            salary:1, 
+            grade:{
+                //cond:[if,then,else]
+                $cond:{
+                    if:{$gte:["$salary", 1500]},
+                    then:"Grade A",
+                    else:"Grade B",
+                }
+            },
+        },
+    },
+]);
+
+
+db.employees.aggregate([
+    {
+        $project:{
+            _id: 0,
+            name:1,
+            salary:1, 
+            grade:{
+                $switch:{
+                    branches:[
+                        {case: { $gte:["$salary", 1500]},then:"Grade A"},
+                        {case: { $lt:["$salary", 1500]},then:"Grade B"}, //lt = less than
+                    ],
+                    default: "Unknwon",
+                },
             },
         },
     },
